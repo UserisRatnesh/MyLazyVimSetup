0f4ba5f41c95b90688bf67babafa5531e6696fe1
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-07-15 13:42:16 +0530

    Updated todo comments.

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -9,8 +9,10 @@ return {
         keywords = {
           TODO = { icon = " ", color = "info" },
           FIXME = { icon = " ", color = "error" },
-          TOPIC = { icon = " ", color = "#FF9E64" },
+          TOPIC = { icon = "➤ ", color = "#FF9E64" },
           NOTE = { icon = " ", color = "hint" },
+          OPTIMAL = { icon = " ", color = "#98C379" },
+          BRUTE = { icon = " ", color = "#E06C75" },
           HACK = { icon = " ", color = "#FFD700" },
           TC = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
         },
